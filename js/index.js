49f71b3f5a9b7bd40485bacf7fe5b36498e45044
loneliness-mahadi <lonelinessmahadi@gmail.com>
2022-09-05 20:33:42 +0600

    all ok

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 
 const apiKey = "aa6ea938956a570b463fa4a1a58898b7"
 
-const data = (city) => {
+const datasValue = (city) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
         .then(res => res.json())
-        .then(data => funtion(data))
+        .then(data => funtion(data)).catch(error => console.log(error))
 
 }
 
@@ -21,7 +21,7 @@ const funtion = (data) => {
         watherSetuation.innerText = ''
     }
     else {
-        console.log(data)
+
         cityName.innerText = data.name
         wather.innerHTML = `${data.main.temp}  &deg;C`
         watherSetuation.innerText = data.weather[0].description
@@ -32,13 +32,28 @@ const funtion = (data) => {
 
 }
 
-
-
-const myFunction = () => {
+const common = () => {
 
     const inputFild = document.getElementById('input-field')
-    data(inputFild.value)
-    inputFild.value = ''
+    datasValue(inputFild.value)
+    console.log(inputFild.value)
 
 }
+
+
+
+
+// When I Click In A button 
+document.getElementById('input-field').addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        common()
+
+    }
+});
+
+
+document.getElementById('button').addEventListener('click', function () {
+    common()
+})
+
 
